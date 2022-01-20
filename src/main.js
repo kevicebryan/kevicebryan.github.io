8a8457abdca194e16fe9603992a3193357d87e5f
kevicebryan <70892991+kevicebryan@users.noreply.github.com>
2022-01-20 14:10:48 +0700

    editedheader
    added logo to replace KevinBryan

--- src/main.js
+++ src/main.js
@@ -2,12 +2,23 @@
 
 const btn = document.querySelector(".mobile-menu-button");
 const sidebar = document.getElementById("sidebar");
+const welcomeTitle = document.getElementById("welcome-title");
 
 btn.addEventListener('click', ()=>{
   if(sidebar.style.display === "none"){
     sidebar.style.display = "block";
+    welcomeTitle.style.opacity = "0.1"
   }else{
     sidebar.style.display = "none";
+    welcomeTitle.style.opacity = "1"
   }
 })
 
+
+function chgImg(x) {
+  x.src = "./img/kevinbryan_stars.png";
+  }
+  
+  function normalImg(x) {
+  x.src = "./img/kevinbryan_chromatic.png";
+  }
